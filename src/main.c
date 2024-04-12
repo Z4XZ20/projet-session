734708ea65791dc16fe17a28646b40ee1dd2b3c4
Alan Jadiman <alan.jadiman@uqtr.ca>
2024-04-11 19:48:04 -0700

    Ajoute indentation

--- src/main.c
+++ src/main.c
@@ -1,32 +1,28 @@
 #include <stdio.h>
 #include "../src/tools.h"
 
-
-void main() {
-
-int nb_ligne=0;
-int error_flag=0;
-
-/*check if the .txt is empty*/
-nb_ligne=checkfile_lenght();
-
-if(nb_ligne==0)
-{   
-    /*if empty then we write database*/
-    write_database();
-}
-
-/*putting the database in RAM for easier mpdification*/
-
-Database *livre = (Database*)malloc(nb_ligne * sizeof(Database));
-
-if(livre == NULL)
+void main()
 {
-    printf("error");
-}
+    int nb_ligne=0;
+    int error_flag=0;
 
-store_data(livre,nb_ligne);
+    /*check if the .txt is empty*/
+    nb_ligne=checkfile_lenght();
 
+    if(nb_ligne==0)
+    {   
+        /*if empty then we write database*/
+        write_database();
+    }
 
+    /*putting the database in RAM for easier mpdification*/
 
+    Database *livre = (Database*)malloc(nb_ligne * sizeof(Database));
+
+    if(livre == NULL)
+    {
+        printf("error");
+    }
+
+    store_data(livre,nb_ligne);
 } 
